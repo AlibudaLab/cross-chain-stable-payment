924314afb0ac54b60564ccffee2bd452e8aee12d
ryanycw <ryan@ryanycw.dev>
2024-07-13 15:56:27 +0200

    build: finish v1 request page

--- web/app/payment/_components/FormPayment.tsx
+++ web/app/payment/_components/FormPayment.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from 'react';
+import { useCallback, useEffect } from 'react';
 import {
   Modal,
   ModalContent,
@@ -7,11 +7,12 @@ import {
   ModalFooter,
   Button,
   useDisclosure,
-  Select, 
+  Select,
   SelectItem,
   Input,
 } from '@nextui-org/react';
 import { parseEther } from 'viem';
+import { useAccount } from 'wagmi';
 import { useBuyMeACoffeeContract } from '../_contracts/useBuyMeACoffeeContract';
 import useFields from '../_hooks/useFields';
 import TransactionSteps from './TransactionSteps';
@@ -20,41 +21,52 @@ import useSmartContractForms from './useSmartContractForms';
 const GAS_COST = 0;
 
 const initFields = {
-  name: '',
-  twitterHandle: '',
-  message: '',
-  coffeeCount: 1,
+  sender: '',
+  receiver: '',
+  sourceChain: 'baseSepolia',
+  destinationChain: 'arbitrumSepolia',
+  amount: 0,
 };
 
 type Fields = {
-  name: string;
-  twitterHandle: string;
-  coffeeCount: number;
-  message: string;
+  sender: string;
+  receiver: string;
+  sourceChain: string;
+  destinationChain: string;
+  amount: number;
 };
 
 function FormPayment() {
+  const { address } = useAccount();
+
   const {
     isOpen: isReceiveOpen,
     onOpen: onReceiveOpen,
     onOpenChange: onReceiveOpenChange,
   } = useDisclosure();
+  const { isOpen: isSendOpen, onOpenChange: onSendOpenChange } = useDisclosure();
 
   const contract = useBuyMeACoffeeContract();
 
-  const { fields, /*setField,*/ resetFields } = useFields<Fields>(initFields);
+  const { fields, setField, resetFields } = useFields<Fields>(initFields);
+
+  useEffect(() => {
+    if (fields.sender === '' && address !== undefined) {
+      setField('sender', address as string);
+    }
+  }, [address, fields.sender, setField]);
 
   const reset = useCallback(async () => {
     resetFields();
   }, [resetFields]);
 
-  const { transactionState, resetContractForms/*, onSubmitTransaction, disabled*/ } =
+  const { transactionState, resetContractForms /*, onSubmitTransaction, disabled*/ } =
     useSmartContractForms({
-      gasFee: parseEther(String(GAS_COST * fields.coffeeCount)),
+      gasFee: parseEther(String(GAS_COST * fields.amount)),
       contract,
       name: 'buyCoffee',
-      arguments: [fields.coffeeCount, fields.name, fields.twitterHandle, fields.message],
-      enableSubmit: fields.name !== '' && fields.message !== '',
+      arguments: [],
+      enableSubmit: fields.sender !== '',
       reset,
     });
 
@@ -62,7 +74,7 @@ function FormPayment() {
     return (
       <TransactionSteps
         transactionStep={transactionState}
-        coffeeCount={fields.coffeeCount}
+        coffeeCount={fields.amount}
         resetContractForms={resetContractForms}
         gasCost={GAS_COST}
       />
@@ -70,16 +82,35 @@ function FormPayment() {
   }
 
   const chains = [
-    {key: "arbitrumSepolia", label: "Arbitrum Sepolia"},
-    {key: "baseSepolia", label: "Base Sepoloa"},
+    { key: 'arbitrumSepolia', label: 'Arbitrum Sepolia' },
+    { key: 'baseSepolia', label: 'Base Sepolia' },
   ];
 
   return (
-    <>
-      <Button 
-        onPress={onReceiveOpen} 
+    <div className="mx-auto flex w-full max-w-md flex-col items-center space-y-6">
+      <Select
+        className="w-full"
+        defaultSelectedKeys={[fields.sourceChain]}
+        onChange={(e) => setField('sourceChain', e.target.value)}
+        variant="underlined"
+      >
+        {chains.map((chain) => (
+          <SelectItem key={chain.key}>{chain.label}</SelectItem>
+        ))}
+      </Select>
+
+      <div className="w-full text-center">
+        <p className="text-lg">
+          {address ? address.slice(0, 6) + '...' + address.slice(-4) : 'Connect Wallet'}
+        </p>
+        <p className="mt-2 text-xl">Your Balance</p>
+        <h2 className="mt-1 text-4xl font-bold">${45}</h2>
+      </div>
+
+      <Button
+        onPress={onReceiveOpen}
         color="primary"
-        className="px-8 py-3 text-lg font-semibold"
+        className="w-full px-12 py-3 text-lg font-semibold"
         radius="full"
       >
         Request
@@ -90,16 +121,6 @@ function FormPayment() {
             <>
               <ModalHeader className="flex flex-col gap-1">Request USDC</ModalHeader>
               <ModalBody>
-              <Select 
-                label="Select an animal" 
-                className="max-w-xs" 
-              >
-                {chains.map((chain) => (
-                  <SelectItem key={chain.key}>
-                    {chain.label}
-                  </SelectItem>
-                ))}
-              </Select>
                 <Input autoFocus label="Email" placeholder="Enter your email" variant="bordered" />
                 <Input
                   label="Password"
@@ -119,16 +140,8 @@ function FormPayment() {
             </>
           )}
         </ModalContent>
-      </Modal>      
-      <Button 
-        onPress={onReceiveOpen} 
-        color="primary"
-        className="px-8 py-3 text-lg font-semibold"
-        radius="full"
-      >
-        Send
-      </Button>
-      <Modal isOpen={isReceiveOpen} onOpenChange={onReceiveOpenChange} placement="top-center">
+      </Modal>
+      <Modal isOpen={isSendOpen} onOpenChange={onSendOpenChange} placement="top-center">
         <ModalContent>
           {(onClose) => (
             <>
@@ -147,14 +160,14 @@ function FormPayment() {
                   Close
                 </Button>
                 <Button color="primary" onPress={onClose}>
-                  Finish Request 
+                  Finish Request
                 </Button>
               </ModalFooter>
             </>
           )}
         </ModalContent>
       </Modal>
-    </>
+    </div>
   );
 }
 
